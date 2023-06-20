611df1124b860f4bd708aa4cfcd7e6a6fd613b45
Hussain Mohd Raza <64502001+hussain-raza@users.noreply.github.com>
2023-06-20 19:32:36 +0530

    Add files via upload

---
+++ streamlit_ui.py
@@ -0,0 +1,95 @@
+import streamlit as st
+import joblib
+import pandas as pd
+
+st.title("10 YEAR HEART DISEASE PREDICTION")
+
+st.write("By- HUSSAIN MOHD RAZA")
+
+#gender = st.selectbox("Enter your gender",["Male", "Female"])
+
+col1, col2, col3 = st.columns(3)
+
+# getting user input
+
+gender = col1.selectbox("Enter your gender",["Male", "Female"])
+
+age = col2.number_input("Enter your age")
+education = col3.selectbox("Highest academic qualification",["High school diploma", "Undergraduate degree", "Postgraduate degree", "PhD"])
+
+isSmoker = col1.selectbox("Are you currently a smoker?",["Yes","No"])
+
+yearsSmoking = col2.number_input("Number of daily cigarettes")
+
+BPMeds = col3.selectbox("Are you currently on BP medication?",["Yes","No"])
+
+stroke = col1.selectbox("Have you ever experienced a stroke?",["Yes","No"])
+
+hyp = col2.selectbox("Do you have hypertension?",["Yes","No"])
+
+diabetes = col3.selectbox("Do you have diabetes?",["Yes","No"])
+
+chol = col1.number_input("Enter your cholesterol level")
+
+sys_bp = col2.number_input("Enter your systolic blood pressure")
+
+dia_bp = col3.number_input("Enter your diastolic blood pressure")
+
+bmi = col1.number_input("Enter your BMI")
+
+heart_rate = col2.number_input("Enter your resting heart rate")
+
+glucose = col3.number_input("Enter your glucose level")
+
+# creating a button for prediction
+
+#st.button('Predict')
+
+# Feature Engineering
+# Transforming Input data of Yes/no from User in numbers(0 and 1)
+
+df_pred = pd.DataFrame([[gender,age,education,isSmoker,yearsSmoking,BPMeds,stroke,hyp,diabetes,chol,sys_bp,dia_bp,bmi,heart_rate,glucose]],
+
+columns= ['male','age','education','currentSmoker','cigsPerDay','BPMeds','prevalentStroke','prevalentHyp','diabetes','totChol','sysBP','diaBP','BMI','heartRate','glucose'])
+
+df_pred['male'] = df_pred['male'].apply(lambda x: 1 if x == 'Male' else 0)
+
+df_pred['prevalentHyp'] = df_pred['prevalentHyp'].apply(lambda x: 1 if x == 'Yes' else 0)
+
+df_pred['prevalentStroke'] = df_pred['prevalentStroke'].apply(lambda x: 1 if x == 'Yes' else 0)
+
+df_pred['diabetes'] = df_pred['diabetes'].apply(lambda x: 1 if x == 'Yes' else 0)
+
+df_pred['BPMeds'] = df_pred['BPMeds'].apply(lambda x: 1 if x == 'Yes' else 0)
+
+df_pred['currentSmoker'] = df_pred['currentSmoker'].apply(lambda x: 1 if x == 'Yes' else 0)
+
+# Function to transform education in numbers
+def transform(data):
+    result = 3
+    if(data=='High school diploma'):
+        result = 0
+    elif(data=='Undergraduate degree'):
+        result = 1
+    elif(data=='Postgraduate degree'):
+        result = 2
+    return(result)
+
+df_pred['education'] = df_pred['education'].apply(transform)
+
+# Saving a model using joblib
+
+model = joblib.load('Heart-Disease-Pred-Model.pkl')
+prediction = model.predict(df_pred)
+
+# Making Predictions
+
+if st.button('Predict'):
+
+    if(prediction[0]==0):
+        st.success("You likely will not develop heart disease in 10 years.")
+        st.balloons()
+        st.image("S.png")
+    else:
+        st.error("You are likely to develop heart disease in 10 years.")
+        st.image("F.jpg")
